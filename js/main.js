d543657d0f737b6c713b7ea50f0425ef4577e001
Shawn <134296052+shawnvera@users.noreply.github.com>
2023-06-23 07:19:23 -0400

    updated

--- js/main.js
+++ js/main.js
@@ -1,96 +1,93 @@
 // event listeners
-window.addEventListener("load", buildUI);
-// restart.addEventListener("click", init);
+window.addEventListener("load", init);
+
 
 // global variables
 let ticTacApp = document.getElementById("app");
-function clickMe(e) {
-    console.log("you clicked " + e.target.id);
+let btn;
+
+let ticTacToe = {
+    header: "Tic-Tac-Toe",
+    classList: "text-center",
+    currentPlayer: 0,
+    playerX: 0,
+    playerO: 1,
+    playerT: null,
+    gameBoard: [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ],
 }
 
-function buildUI() {
+// functions
 
-    var board = {
-        id: "board",
-        type: "div",
-        listener: clickMe,
-        parent: "app",
-        classList: "container"
-    }
+//restart button
+function restart() {
+    location.reload();
 }
-// var gameBoard = generateHTMLElement(board);
-// console.log(board)
-// }
 
-// event listeners
-window.addEventListener("load", buildUI);
-// restart.addEventListener("click", init);
-
-// function generateHTMLElement(board) {
-//     let element = document.createElement(board.type);
-//     element.classList = board.classList;
-//     if (board.id) {
-//         element.id = board.id;
-//     }
-//     if (board.parent) {
-//         board.parent.appendChild(element);
-//     }
-//     if (board.listener) {
-//         element.addEventListener('click', board.listener);
-//     }
-//     if (board.style) {
-//         element.style = board.style;
-//     }
-//     return element;
-// }
-
-
-
-
-
-
-
-
-function startGame() {
+function init() {
     let box;
     let row;
     let col;
     let col1;
     let col2;
-    
+    let h1;
+let h2;
+    h1 = document.createElement("div");
+    h2 = document.createElement("h2");
+    h1.classList = "text-center"
+    h1.textContent = ticTacToe.header;
+    ticTacApp.appendChild(h2);
+    h2.appendChild(h1);
 
+    box = document.createElement("div");
+    box.classList = "box p-5 border border-dark";
 
-    for (let i = 0; i < 9; i++) {
-        box = document.createElement("div");
-        box.classList = "box p-5 border border-dark";
-box.style = "border-left; 0; border-top: 0";
+    // could I use a .forEach to simplify this?
 
+    for (let i = 0; i < 3; i++) {
 
-        // row = document.createElement("div");
-        
-        // col = document.createElement("div");
-        // col1 = document.createElement("div");
-        // col2 = document.createElement("div");
-        
-        // row.classList = "row";
-        
-        // col.classList = "col";
-        // col1.classList = "col";
-        // col2.classList = "col";
-        
+        row = document.createElement("div");
+
+        col = document.createElement("div");
+        col1 = document.createElement("div");
+        col2 = document.createElement("div");
+
+        row.classList = "row border border-dark";
+
+        col.classList = "col-4 p-5 border border-dark";
+        col1.classList = "col-4 p-5 border border-dark";
+        col2.classList = "col-4 p-5 border border-dark";
+
         ticTacApp.appendChild(box);
-        // box.appendChild(row);
-        // row.appendChild(col);
-        // row.appendChild(col1);
-        // row.appendChild(col2);
+        box.appendChild(row);
+        row.appendChild(col);
+        row.appendChild(col1);
+        row.appendChild(col2);
 
-
-
-        // box.addEventListener("click", );
+        col.addEventListener("click", clickMe, { once: true });
+        col1.addEventListener("click", clickMe, { once: true });
+        col2.addEventListener("click", clickMe, { once: true });
     }
 
+    btn = document.createElement("div");
+    btn.classList = "btn p-3 btn-primary";
+    btn.textContent = "Restart";
+    ticTacApp.appendChild(btn);
+    btn.addEventListener("click", restart);
 }
-startGame();
+
+function clickMe(e) {
+    if(col==="clicked"){
+        col.display = ticTacToe.currentPlayer;
+    currentPlayer = 1;
+    ticTacToe.gameBoard.push = e.target;
+    console.log(ticTacToe.gameBoard);
+
+}
+}
 
 
 /*  ticTacApp.forEach((block, index) => {
@@ -136,3 +133,34 @@ gameBoard = document.createElement("div");
 //     ?? .map each block on the DOM
 //     DISPLAY restart BUTTON from state
 //
+// function generateHTMLElement(board) {
+//     let element = document.createElement(board.type);
+//     element.classList = board.classList;
+//     if (board.id) {
+//         element.id = board.id;
+//     }
+//     if (board.parent) {
+//         board.parent.appendChild(element);
+//     }
+//     if (board.listener) {
+//         element.addEventListener('click', board.listener);
+//     }
+//     if (board.style) {
+//         element.style = board.style;
+//     }
+//     return element;
+// }
+// var gameBoard = generateHTMLElement(board);
+// console.log(board)
+// }
+
+// function buildUI() {
+
+//     var board = {
+//         id: "board",
+//         type: "div",
+//         listener: clickMe,
+//         parent: "app",
+//         classList: "container"
+//     }
+// }
